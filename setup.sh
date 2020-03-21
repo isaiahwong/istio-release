e204e02547bd7dfe144571bd3aef4eb5fe352f3e
isaiahwong <iisaiah.dev@gmail.com>
2020-03-22 00:48:41 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 istioctl manifest apply --set values.kiali.enabled=true,\
 values.global.mtls.enabled=true,\
+values.grafana.enabled=true,\
 values.global.controlPlaneSecurityEnabled=true,\
-values.sidecarInjectorWebhook.rewriteAppHTTPProbe=true
--f ./release/resource.yaml  
+values.sidecarInjectorWebhook.rewriteAppHTTPProbe=true,\
+values.global.proxy.accessLogFile="/dev/stdout"
